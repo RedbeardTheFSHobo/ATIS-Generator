b4fbf9028aa34e22cef841e3275cb26f9b35123d
RedbeardTheFSHobo <89643099+RedbeardTheFSHobo@users.noreply.github.com>
2021-10-18 08:46:14 -0400

    Update download.php
    Updated to work with index.php update.

--- download.php
+++ download.php
@@ -1,5 +1,5 @@
 <?
-$ch = curl_init("http://api.voicerss.org/?key=92a278f391ff4c4fb65e6fbc69c10e5f&hl=en-us&c=MP3&v=John&f=16khz_16bit_stereo&src=" . rawurlencode($_GET['output']));
+$ch = curl_init("http://api.voicerss.org/?key=92a278f391ff4c4fb65e6fbc69c10e5f&hl=en-us&c=MP3&v=John&f=16khz_16bit_stereo&src=" . rawurlencode($_POST['spokenATIS']));
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_NOBODY, 0);
 curl_setopt($ch, CURLOPT_TIMEOUT, 5);
